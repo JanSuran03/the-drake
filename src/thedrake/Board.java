fc88bc907305e0a07278d4c361bf2c8794746189
Jan Šuráň <suran.orgpad@gmail.com>
2023-03-06 20:45:17 +0100

    Task 2: Implemented the Board class.

--- src/thedrake/Board.java
+++ src/thedrake/Board.java
@@ -1,20 +1,43 @@
 package thedrake;
 
 public class Board {
+    private final BoardTile[][] boardTiles;
+    private final int dimension;
+
 
     public Board(int dimension) {
+        this.dimension = dimension;
+        boardTiles = new BoardTile[dimension][dimension];
+        for (int i = 0; i < dimension; i++)
+            for (int j = 0; j < dimension; j++)
+                boardTiles[i][j] = BoardTile.EMPTY;
     }
 
     public int dimension() {
+        return dimension;
     }
 
     public BoardTile at(TilePos pos) {
+        return boardTiles[pos.i()][pos.j()];
+    }
+
+    @Override
+    protected Board clone() {
+        Board clone = new Board(dimension);
+        for (int i = 0; i < dimension; i++)
+            System.arraycopy(this.boardTiles[i], 0, clone.boardTiles[i], 0, dimension);
+        return clone;
     }
 
     public Board withTiles(TileAt... ats) {
+        Board clone = clone();
+        for (TileAt tile : ats)
+            clone.boardTiles[tile.pos.i()][tile.pos.j()] = tile.tile;
+        return clone;
     }
 
     public PositionFactory positionFactory() {
+        return new PositionFactory(dimension);
     }
 
     public static class TileAt {
