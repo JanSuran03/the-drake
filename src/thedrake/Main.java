f387869b49cf2c82a2e9bd165bfc2beedca25d0b
Jan Šuráň <suran.orgpad@gmail.com>
2023-05-25 17:58:28 +0200

    Task 6: Add the main entry of the application.

---
+++ src/thedrake/Main.java
@@ -0,0 +1,12 @@
+package thedrake;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("The Drake");
+        stage.show();
+    }
+}
