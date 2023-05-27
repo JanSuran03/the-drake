ce0de518f59bdcc8a6738a156aec30ff36403573
Jan Šuráň <suran.orgpad@gmail.com>
2023-05-27 16:46:14 +0200

    Task 6: Add basic FXML.

--- src/thedrake/ui/MenuView.java
+++ src/thedrake/ui/MenuView.java
@@ -1,17 +1,27 @@
 package thedrake.ui;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class MenuView extends VBox {
-    final Stage stage ;
+    final Stage stage;
+
     public MenuView(Stage stage) {
         this.getStyleClass().add("menu-view");
         this.setAlignment(javafx.geometry.Pos.CENTER);
         this.getChildren().add(new Button("Play"));
         this.getChildren().add(new Button("Play vs AI"));
-        this.getChildren().add(new Button("Play online"));
+        try {
+            this.getChildren().add(new FXMLLoader().load(Files.newInputStream(Paths.get("resources/fxml/credits.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         Button quitButton = new Button("Quit");
         quitButton.setOnAction(e -> stage.close());
         this.getChildren().add(quitButton);
