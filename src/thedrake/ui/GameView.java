6ccbb49a35ecf73522c21def7166dc1f2827df21
Jan Šuráň <suran.orgpad@gmail.com>
2023-05-29 14:18:37 +0200

    Task 7: Add a very basic stack view.

--- src/thedrake/ui/GameView.java
+++ src/thedrake/ui/GameView.java
@@ -3,14 +3,26 @@ package thedrake.ui;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import thedrake.*;
 
 public class GameView extends VBox {
-    public GameView() {
-        HBox stack1 = new HBox();
-        stack1.getChildren().add(new Label("Blue:"));
-        stack1.getChildren().add(new Label("Foo"));
+    GameState gameState;
 
-        HBox stack2 = new HBox();
+    public GameView() {
+        gameState = createGameState();
+        HBox stack = new HBox();
+        var stk = gameState.army(PlayingSide.BLUE).stack();
+        for (var troop : stk) {
+            System.out.println(troop.name());
+        }
+        StackView stackView = new StackView(PlayingSide.BLUE, gameState.army(PlayingSide.BLUE).stack());
+        stackView.update();
+        StackView stackView2 = new StackView(PlayingSide.ORANGE, gameState.army(PlayingSide.ORANGE).stack());
+        stackView2.update();
+        this.getChildren().add(stackView);
+        this.getChildren().add(stackView2);
+
+        /*HBox stack2 = new HBox();
         stack2.getChildren().add(new Label("Orange:"));
         stack2.getChildren().add(new Label("Bar"));
 
@@ -19,6 +31,13 @@ public class GameView extends VBox {
 
         getChildren().add(stack1);
         getChildren().add(boardView);
-        getChildren().add(stack2);
+        getChildren().add(stack2);*/
+    }
+
+    public GameState createGameState() {
+        Board board = new Board(4);
+        PositionFactory pf = board.positionFactory();
+        board = board.withTiles(new Board.TileAt(pf.pos("a2"), BoardTile.MOUNTAIN));
+        return new StandardDrakeSetup().startState(board);
     }
 }
---
+++ src/thedrake/ui/StackView.java
@@ -0,0 +1,34 @@
+package thedrake.ui;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import thedrake.PlayingSide;
+import thedrake.Troop;
+
+import java.util.List;
+
+public class StackView extends HBox {
+    private static final String side = "front";
+    List<Troop> stack;
+
+    PlayingSide playingSide;
+
+    public StackView(PlayingSide playingSide, List<Troop> stack) {
+        this.stack = stack;
+        this.playingSide = playingSide;
+    }
+
+    public void update() {
+        this.getChildren().clear();
+        for (var troop : stack) {
+            System.out.println(getClass().getResource("/images/" + side + troop.name()
+                    + (playingSide == PlayingSide.BLUE ? 'B' : 'O') + ".png").toExternalForm());
+            this.getChildren().add(new ImageView(getClass().getResource("/images/" + side + troop.name()
+                    + (playingSide == PlayingSide.BLUE ? 'B' : 'O') + ".png").toExternalForm()));
+        }
+    }
+
+    static class TroopView {
+
+    }
+}
